9330f712fc27e01f22f8a048bdffef5ed363b955
Ali Yousefi <ali.visual.studio@gmail.com>
2023-11-24 00:15:51 +0330

    Add GetByIdAndLanguage

---
+++ src/CSharp/EasyMicroservices.CustomerMicroservice.Domain/Contracts/Requests/GetByIdAndLanguageRequestContract.cs
@@ -0,0 +1,9 @@
+﻿using EasyMicroservices.Cores.Contracts.Requests;
+
+namespace EasyMicroservices.CustomerMicroservice.Contracts.Requests
+{
+    public class GetByIdAndLanguageRequestContract<T> : GetIdRequestContract<T>
+    {
+        public string Language { get; set; }
+    }
+}
